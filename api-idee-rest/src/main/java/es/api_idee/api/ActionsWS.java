1d679392f6c99dfee8c11df2514e181a231f003d
gdtelcnig_develop <gdtelcnig_develop@guadaltel.com>
2025-01-31 11:39:55 +0100

    Eliminacion de configuracion de temas y su uso

--- api-idee-rest/src/main/java/es/api_idee/api/ActionsWS.java
+++ api-idee-rest/src/main/java/es/api_idee/api/ActionsWS.java
@@ -36,8 +36,8 @@ public class ActionsWS {
 	private ResourceBundle configProperties = ResourceBundle.getBundle("configuration");
 
 	/*
-	 * # services services=${services} # theme theme.urls=${theme.urls}
-	 * theme.names=${theme.names} # projection projection=${api-idee.proj.default}
+	 * # services services=${services}
+	 * # projection projection=${api-idee.proj.default}
 	 */
 
 	/**
@@ -54,7 +54,6 @@ public class ActionsWS {
 		actions.put("/controls");
 		actions.put("/services");
 		actions.put("/version");
-		actions.put("/themes");
 		actions.put("/projection");
 		actions.put("/plugins");
 		actions.put("/resources/svg");
@@ -109,28 +108,6 @@ public class ActionsWS {
 		}
 
 		return JSBuilder.wrapCallback(servicesJSON, callbackFn);
-	}
-
-	/**
-	 * Returns the available themes for the user
-	 * 
-	 * @param callbackFn the name of the javascript function to execute as callback
-	 * 
-	 * @return the javascript code
-	 */
-	@GET
-	@Path("/themes")
-	public String showAvailableThemes(@QueryParam("callback") String callbackFn) {
-		String themesRaw = configProperties.getString("themes");
-		String[] themes = themesRaw.split(",");
-
-		JSONArray themesJSON = new JSONArray();
-
-		for (String theme : themes) {
-			themesJSON.put(theme);
-		}
-
-		return JSBuilder.wrapCallback(themesJSON, callbackFn);
 	}
 
 	/**
