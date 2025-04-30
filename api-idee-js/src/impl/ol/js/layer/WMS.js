4c4e9877172bc243847e7cb43654570649bfed86
gdtelcnig_develop <gdtelcnig_develop@guadaltel.com>
2025-04-30 14:26:30 +0200

    105: Capa WMS - ADDED_TO_MAP

--- api-idee-js/src/impl/ol/js/layer/WMS.js
+++ api-idee-js/src/impl/ol/js/layer/WMS.js
@@ -262,7 +262,6 @@ class WMS extends LayerBase {
   addTo(map, addLayer = true) {
     this.addLayerToMap_ = addLayer;
     this.map = map;
-    this.fire(EventType.ADDED_TO_MAP);
 
     // calculates the resolutions from scales
     if (!isNull(this.options)
@@ -298,6 +297,7 @@ class WMS extends LayerBase {
         // EXCEPTIONS: 'image/png',
       });
     }
+    this.facadeLayer_?.fire(EventType.ADDED_TO_MAP);
   }
 
   paramsOLLayers() {
