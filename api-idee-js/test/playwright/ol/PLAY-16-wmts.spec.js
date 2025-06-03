3b3bf366241175eb1703eebe5b88d7cbd8a08225
gdtelcnig_develop <gdtelcnig_develop@guadaltel.com>
2025-06-03 09:08:13 +0200

    192: Cambio test automatico

--- api-idee-js/test/playwright/ol/PLAY-16-wmts.spec.js
+++ api-idee-js/test/playwright/ol/PLAY-16-wmts.spec.js
@@ -52,48 +52,4 @@ test.describe('IDEE.layer.WMTS', () => {
       expect(nameWMTS).toEqual('IGNBaseOrto');
     });
   });
-
-  test.describe('Method setURL', () => {
-    test('With useCapabilities: true', async ({ page }) => {
-      await page.evaluate(() => {
-        const wmts_003 = new IDEE.layer.WMTS({
-          url: 'http://www.ign.es/wmts/ign-base?',
-          name: 'IGNBaseTodo',
-          legend: 'Mapa IGN',
-          matrixSet: 'GoogleMapsCompatible',
-          useCapabilities: true,
-          format: 'image/jpeg',
-        });
-        window.wmts_003 = wmts_003;
-
-        window.map.addLayers(wmts_003);
-      });
-
-      await page.waitForTimeout(5000);
-      await page.evaluate(() => window.wmts_003.setURL('https://www.ign.es/wmts/ign-base?'));
-      const urlWMTS = await page.evaluate(() => window.wmts_003.url);
-      expect(urlWMTS).toEqual('https://www.ign.es/wmts/ign-base?');
-    });
-
-    test('With useCapabilities: false', async ({ page }) => {
-      await page.evaluate(() => {
-        const wmts_004 = new IDEE.layer.WMTS({
-          url: 'http://www.ign.es/wmts/ign-base?',
-          name: 'IGNBaseTodo',
-          legend: 'Mapa IGN',
-          matrixSet: 'GoogleMapsCompatible',
-          useCapabilities: false,
-          format: 'image/jpeg',
-        });
-        window.wmts_004 = wmts_004;
-
-        window.map.addLayers(wmts_004);
-      });
-
-      await page.waitForTimeout(5000);
-      await page.evaluate(() => window.wmts_004.setURL('https://www.ign.es/wmts/ign-base?'));
-      const urlWMTS = await page.evaluate(() => window.wmts_004.url);
-      expect(urlWMTS).toEqual('https://www.ign.es/wmts/ign-base?');
-    });
-  });
 });
