89d3fc023e35c9663b291a73caba7672a1a18c08
Alberto <albertoss@alumnos.iesgalileo.es>
2025-06-11 11:16:20 +0200

    feat: añade posibilidad de cargar plantillas personalizadas en el primer modo del plugin printviewmanagement

---
+++ api-idee-js/src/plugins/printviewmanagement/src/impl/ol/js/templateCustomizer.js
@@ -0,0 +1,70 @@
+export default class TemplateCustomizer extends IDEE.impl.Control {
+  /**
+   * @classdesc
+   * Main constructor of the measure conrol.
+   *
+   * @constructor
+   * @api stable
+   */
+  constructor(map) {
+    super();
+    /**
+     * Facade of the map
+     * @private
+     * @type {IDEE.Map}
+     */
+    this.facadeMap_ = map;
+
+    this.errors = [];
+  }
+
+  /**
+   * This function adds the control to the specified map
+   *
+   * @public
+   * @function
+   * @param {IDEE.Map} map to add the plugin
+   * @param {function} template template of this control
+   * @api stable
+   */
+  addTo(map, element) {
+    this.facadeMap_ = map;
+    this.element = element;
+    map.getMapImpl().addControl(this);
+  }
+
+  /**
+   * Función auxiliar para obtener la resolución de un punto
+   * @param {string} projection Código de la proyección
+   * @param {number} dpi DPI
+   * @param {Array<number>} point Punto [x, y]
+   * @return {number} Resolución
+   */
+  getPointResolution(projection, dpi, point) {
+    return ol.proj.getPointResolution(projection, dpi / 25.4, point);
+  }
+
+  /**
+   * Función auxiliar para transformar coordenadas entre proyecciones
+   * @param {Array<number>} coordinates Coordenadas [x, y]
+   * @param {string} sourceProjection Proyección origen
+   * @param {string} targetProjection Proyección destino
+   * @return {Array<number>} Coordenadas transformadas
+   */
+  transformCoordinates(coordinates, sourceProjection, targetProjection) {
+    return ol.proj.transform(coordinates, sourceProjection, targetProjection);
+  }
+
+  getMetersPerUnit(units) {
+    return ol.proj.Units.METERS_PER_UNIT[units];
+  }
+
+  /**
+   * Función auxiliar para crear una nueva vista
+   * @param {Object} options Opciones de la vista
+   * @return {ol.View} Nueva vista
+   */
+  createView(options) {
+    return new ol.View(options);
+  }
+}
