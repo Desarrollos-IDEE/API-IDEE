d1be133e57428592de0fdca6c3d12f9775ff06de
gdtelcnig_develop <gdtelcnig_develop@guadaltel.com>
2025-07-29 11:56:17 +0200

    Test automatico capa y eventos GeoPackage

--- api-idee-js/test/playwright/ol/PLAY-12-GeoPackage.spec.js
+++ api-idee-js/test/playwright/ol/PLAY-12-GeoPackage.spec.js
@@ -1,27 +1,49 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('GeoPackage Layer', () => {
-  let map;
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/test/playwright/ol/basic-ol.html');
-    await page.evaluate(() => {
-      map = IDEE.map({ container: 'map' });
-      window.map = map;
-    });
+test('Capa GeoPackage', async ({ page }) => {
+  let addGeoPackage = false;
+  let addGeoPackageTile = false;
+
+  await page.goto('/test/playwright/ol/basic-ol.html');
+
+  page.on('console', (msg) => {
+    if (msg.type() === 'log' && msg.text() === 'Capa GeoPackage añadida') {
+      addGeoPackage = true;
+    }
+
+    if (msg.type() === 'log'
+      && msg.text() === 'Capa GeoPackageTile añadida') {
+      addGeoPackageTile = true;
+    }
   });
 
-  test('Add GeoPackage', async ({ page }) => {
-    await page.evaluate(() => {
-      window.fetch('/test/playwright/ol/PLAY-12-GeoPackage.spec.js-file/rivers.gpkg').then((response) => {
-        const gpkg = new IDEE.layer.GeoPackage(response);
+  await page.evaluate(() => {
+    const mapjs = IDEE.map({
+      container: 'map',
+    });
+    window.mapjs = mapjs;
+  });
 
-        map.addGeoPackage([gpkg]);
-        window.gpkg = gpkg;
-        const typeLayerVector = map.getLayers()[1].type;
-        expect(typeLayerVector).toEqual('GeoJSON');
-        const typeLayerTile = map.getLayers()[2].type;
-        expect(typeLayerTile).toEqual('GeoPackageTile');
+  await page.evaluate(() => {
+    window.fetch('https://componentes.idee.es/estaticos/Datos/gpkg/rivers.gpkg').then((response) => {
+      const gpkg = new IDEE.layer.GeoPackage(response);
+
+      window.mapjs.on(IDEE.evt.ADDED_GEOPACKAGE, () => {
+        console.log('Capa GeoPackage añadida');
       });
+
+      window.mapjs.on(IDEE.evt.ADDED_GEOPACKAGE_TILE, () => {
+        console.log('Capa GeoPackageTile añadida');
+      });
+
+      window.mapjs.addGeoPackage([gpkg]);
     });
   });
+  await page.waitForTimeout(5000);
+  expect(addGeoPackage).toBe(true);
+  expect(addGeoPackageTile).toBe(true);
+  const typeLayerVector = await page.evaluate(() => window.mapjs.getLayers()[1].type);
+  expect(typeLayerVector).toEqual('GeoJSON');
+  const typeLayerTile = await page.evaluate(() => window.mapjs.getLayers()[2].type);
+  expect(typeLayerTile).toEqual('GeoPackageTile');
 });
