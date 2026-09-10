37673dfe04e2b445b48c8af54e2efd7d30589833
Alberto <albertosamaniego00@gmail.com>
2026-09-10 11:25:20 +0200

    fix: resolves syncronization problem by rebuilding mousesrs plugin

--- api-idee-js/src/plugins/mousesrs/src/impl/ol/js/mousesrscontrol.js
+++ api-idee-js/src/plugins/mousesrs/src/impl/ol/js/mousesrscontrol.js
@@ -157,15 +157,13 @@ export default class MouseSRSControl extends IDEE.impl.Control {
 
     map.getMapImpl().addControl(this.mousePositionControl);
     // super.addTo(map, html);
-    setTimeout(() => {
-      this.mousePositionControl.initLoaderManager(map);
-      document.querySelector('.m-mousesrs-container .m-mouse-srs').setAttribute('role', 'text ');
-      document.querySelector('.m-mousesrs-container .m-mouse-srs').setAttribute('tabIndex', this.order);
-      document.querySelector('.m-mousesrs-container .m-mouse-srs').addEventListener('click', this.openChangeSRS.bind(this, this.auxMap_, html));
-      document.querySelector('.m-mousesrs-container .m-mouse-srs').addEventListener('keydown', ({ key }) => {
-        if (key === 'Enter') this.openChangeSRS(this, this.auxMap_, html);
-      });
-    }, 1000);
+    this.mousePositionControl.initLoaderManager(map);
+    document.querySelector('.m-mousesrs-container .m-mouse-srs').setAttribute('role', 'text ');
+    document.querySelector('.m-mousesrs-container .m-mouse-srs').setAttribute('tabIndex', this.order);
+    document.querySelector('.m-mousesrs-container .m-mouse-srs').addEventListener('click', this.openChangeSRS.bind(this, this.auxMap_, html));
+    document.querySelector('.m-mousesrs-container .m-mouse-srs').addEventListener('keydown', ({ key }) => {
+      if (key === 'Enter') this.openChangeSRS(this, this.auxMap_, html);
+    });
   }
 
   openChangeSRS(map, html) {
@@ -188,67 +186,63 @@ export default class MouseSRSControl extends IDEE.impl.Control {
       this.formatEPSGs(content);
     }
     IDEE.dialog.info(content.outerHTML, getValue('select_srs'), this.order);
-    setTimeout(() => {
-      const input = document.querySelector('#m-mousesrs-epsg-selected');
-      const listElem = document.getElementById('m-mousesrs-srs-selector');
-      let isEditable = false;
+    const input = document.querySelector('#m-mousesrs-epsg-selected');
+    const listElem = document.getElementById('m-mousesrs-srs-selector');
+    let isEditable = false;
 
-      listElem.addEventListener('mousedown', (event) => {
-        event.preventDefault();
-      });
+    listElem.addEventListener('mousedown', (event) => {
+      event.preventDefault();
+    });
 
-      input.addEventListener('focus', () => {
-        if (!isEditable) {
-          listElem.style.display = 'block';
-          const list = document.querySelectorAll('#m-mousesrs-srs-selector li a');
-          list.forEach((li) => {
-            li.addEventListener('mousedown', (event) => {
-              event.preventDefault();
-              const value = event.target.getAttribute('value');
-              if (value === 'default') {
-                isEditable = true;
-                input.removeAttribute('readonly');
-                input.value = '';
-                input.placeholder = getValue('placeholder_custom_epsg');
-                listElem.style.display = 'none';
-                input.focus();
-              } else {
-                input.value = value;
-                this.changeSRS(map, html, value);
-              }
-            });
+    input.addEventListener('focus', () => {
+      if (!isEditable) {
+        listElem.style.display = 'block';
+        const list = document.querySelectorAll('#m-mousesrs-srs-selector li a');
+        list.forEach((li) => {
+          li.addEventListener('mousedown', (event) => {
+            event.preventDefault();
+            const value = event.target.getAttribute('value');
+            if (value === 'default') {
+              isEditable = true;
+              input.removeAttribute('readonly');
+              input.value = '';
+              input.placeholder = getValue('placeholder_custom_epsg');
+              listElem.style.display = 'none';
+              input.focus();
+            } else {
+              input.value = value;
+              this.changeSRS(map, html, value);
+            }
           });
-        }
-      });
+        });
+      }
+    });
 
-      input.addEventListener('blur', () => {
-        setTimeout(() => {
-          listElem.style.display = 'none';
-          isEditable = false;
-          if (!input.hasAttribute('readonly')) {
-            input.setAttribute('readonly', 'readonly');
-            input.value = this.srs;
-          }
-        }, 150);
-      });
+    input.addEventListener('blur', () => {
+      listElem.style.display = 'none';
+      isEditable = false;
+      if (!input.hasAttribute('readonly')) {
+        input.setAttribute('readonly', 'readonly');
+        input.value = this.srs;
+      }
+    });
 
-      input.addEventListener('keyup', (event) => {
-        if (isEditable && event.key === 'Enter') {
-          isEditable = false;
-          input.setAttribute('readonly', 'readonly');
-          this.changeSRS(map, html);
-        }
-      });
-      const button = document.querySelector('div.m-dialog.info div.m-button > button');
-      button.innerHTML = getValue('close');
-
-      button.addEventListener('click', () => {
+    input.addEventListener('keyup', (event) => {
+      if (isEditable && event.key === 'Enter') {
         isEditable = false;
-        if (!input.hasAttribute('readonly')) {
-          input.setAttribute('readonly', 'readonly');
-        }
-      });
-    }, 10);
+        input.setAttribute('readonly', 'readonly');
+        this.changeSRS(map, html);
+      }
+    });
+    const button = document.querySelector('div.m-dialog.info div.m-button > button');
+    button.innerHTML = getValue('close');
+
+    button.addEventListener('click', () => {
+      isEditable = false;
+      if (!input.hasAttribute('readonly')) {
+        input.setAttribute('readonly', 'readonly');
+      }
+    });
     if (this.draggableDialog) {
       IDEE.utils.draggabillyElement('.m-dialog .m-modal .m-content', '.m-dialog .m-modal .m-content .m-title');
     }
