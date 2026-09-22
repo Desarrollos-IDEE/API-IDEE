a6dd2adbe76487530dbc226388a4207244d8c90b
gdtelcnig <gdtelcnig_develop@guadaltel.com>
2026-09-22 12:51:48 +0200

    Se arregla collapsible en mapheader

--- api-idee-js/src/plugins/mapheader/src/facade/js/mapheader.js
+++ api-idee-js/src/plugins/mapheader/src/facade/js/mapheader.js
@@ -79,9 +79,13 @@ export default class Mapheader extends IDEE.Plugin {
      * @private
      * @type {boolean}
      */
-    this.collapsible = options.collapsible;
-    if (this.collapsible === undefined) {
+    this.collapsible = true;
+    if (IDEE.utils.isBoolean(options.collapsible)) {
+      this.collapsible = options.collapsible;
+    } else if (options.collapsible === 'true') {
       this.collapsible = true;
+    } else if (options.collapsible === 'false') {
+      this.collapsible = false;
     }
 
     /**
